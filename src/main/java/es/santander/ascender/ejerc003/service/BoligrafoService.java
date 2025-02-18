64c9d19fac26f97a62abbb872320ac48f74fca0d
LauraObreAnto <obregonantonlaura@gmail.com>
2025-02-18 10:19:48 +0100

    correcciondeboli

---
+++ src/main/java/es/santander/ascender/ejerc003/service/BoligrafoService.java
@@ -0,0 +1,48 @@
+package es.santander.ascender.ejerc003.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.santander.ascender.ejerc003.model.Boligrafo;
+import es.santander.ascender.ejerc003.repository.BoligrafoRepository;
+
+@Service
+public class BoligrafoService {
+
+    @Autowired
+    private BoligrafoRepository repository;
+
+    public Boligrafo create(Boligrafo boligrafo) {
+        if (boligrafo.getId() != null) {
+            throw new CrudSecurityException("Han tratado de modificar un registro boligrafo utilizando la creación",
+                                                 CRUDOperation.CREATE, 
+                                                 boligrafo.getId());
+        }
+        return repository.save(boligrafo);
+    }
+
+    public Boligrafo read(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<Boligrafo> read() {
+        return repository.findAll();
+    }
+
+    public Boligrafo update(Boligrafo boligrafo) {
+        if (boligrafo.getId() == null) {
+            throw new CrudSecurityException("Han tratado de crear un registro boligrafo utilizando la modifición",
+                                                 CRUDOperation.UPDATE, 
+                                                 null);
+            
+        }
+        return repository.save(boligrafo);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+        return;
+    }
+}
